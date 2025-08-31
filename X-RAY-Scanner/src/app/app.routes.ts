5afb3ec7a0bb383f8b9e63d91be91ac65cb5216d
MiguelAngelGRomero <miguel_143_gomez@hotmail.com>
2025-08-31 15:07:03 -0600

    Primeros pasos

---
+++ X-RAY-Scanner/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+        import('../modules/home/home.component').then(h => h.HomeComponent),
+    },
+];
