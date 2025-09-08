9de9a4ab5318ada800a91a0bf0a579e0f8951e00
MiguelAngelGRomero <miguel_143_gomez@hotmail.com>
2025-09-07 21:39:21 -0600

    Inicio del Home

--- X-RAY-Scanner/src/app/modules/home/home.component.ts
+++ X-RAY-Scanner/src/app/modules/home/home.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component,  ViewChild, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  template: `
-    <div style="padding: 2rem; text-align: center;">
-      <h1>Welcome to Home!</h1>
-      <p>Login successful!</p>
-    </div>
-  `
+  imports: [CommonModule, FormsModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  // lo que el usuario escribe
+   @ViewChild('messageInput') messageInput!: ElementRef;
+  
+  isMinimized = false;
+  
+  toggleChat() {
+    this.isMinimized = !this.isMinimized;
+  }
+  
+  sendMessage() {
+    const message = this.messageInput.nativeElement.value.trim();
+    if (message) {
+      // Aquí iría la lógica para enviar el mensaje
+      console.log('Mensaje enviado:', message);
+      this.messageInput.nativeElement.value = '';
+    }
+  }
 }
